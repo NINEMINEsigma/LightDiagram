615e016d214987b662a45c6d42749572e477f0db
NINEMINEsigma <1371605831@qq.com>
2024-06-15 22:55:29 +0800

    Create LF_Architecture.h

---
+++ LightDiagram/Core/Header/LF_Architecture.h
@@ -0,0 +1,13 @@
+#ifndef __FILE_LF_ARCHITECTURE
+
+#define __FILE_LF_ARCHITECTURE
+
+#include<Core/Header/LF_Config.h>
+
+namespace ld
+{
+	_LF_API(Class)	bad_null_architectrue Symbol_Push
+	_LF_Inherited(ldex)
+}
+
+#endif // !__FILE_LF_ARCHITECTURE
