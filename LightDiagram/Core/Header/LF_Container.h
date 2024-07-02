b019d57a2edf6810b2bfd37d2f2feae910397387
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2024-07-02 15:41:27 +0800

    Update LF_Container.h
    DefaultController TODO

--- LightDiagram/Core/Header/LF_Container.h
+++ LightDiagram/Core/Header/LF_Container.h
@@ -9,6 +9,7 @@
 
 namespace ld
 {
+#define FourWayPointerLevel public
 	// root(0,0)
 	// --|
 	// first(0,1) ->                        second(1,1) ->                   thrid(2,1)
@@ -32,13 +33,17 @@ namespace ld
 		tag* childs_end_ptr = nullptr;
 		// beyond ptr, when this ptr is leaf, it will be pointing to rightward leaf
 		tag* beyond_next_ptr = nullptr;
+		// beyond ptr, when this ptr is leaf, it will be pointing to leftward leaf
+		tag* beyond_back_ptr = nullptr;
 	public:
-		template<
-			typename Key, typename Value, typename IndexOrderIndicator, typename MemoryIndicator,
-			typename InsertIndicator, typename EarseIndicator
-		>
-		friend class LFContainter;
-	protected:
+		template< typename Key, typename Value, typename MemoryIndicator, typename Controller >
+		friend class LFContainter<Key, Value, MemoryIndicator, Controller>;
+		T data;
+		T& GetData() const
+		{
+			return data;
+		}
+	FourWayPointerLevel:
 #pragma region 
 		//*
 		// Set new linkship: this -> ptr -> old-link right
@@ -72,7 +77,7 @@ namespace ld
 		size_t index_from_head = 0;
 		// index counter from right to left
 		size_t index_from_tail = 0;
-	protected:
+	FourWayPointerLevel:
 #pragma region 
 		//*
 		// Set new linkship: 
@@ -93,7 +98,7 @@ namespace ld
 		size_t index_from_root = 0;
 		// index counter which is distance from this ptr to farthest leaf
 		size_t index_from_farthest_leaf = 0;
-	protected:
+	FourWayPointerLevel:
 #pragma region 
 		// Set new linkship:
 		// this
@@ -110,7 +115,7 @@ namespace ld
 		// Get childs branch tail one
 		tag* GetChildsRight() const;
 #pragma endregion
-	protected:
+		FourWayPointerLevel:
 #pragma region 
 		// Get index from head
 		size_t GetIndex() const;
@@ -123,7 +128,7 @@ namespace ld
 		// Get this sub tree depth(from root to farthest leaf of this ptr's childs)
 		size_t GetTreeDepth() const;
 #pragma endregion
-	protected:
+		FourWayPointerLevel:
 #pragma region
 		// BreakLeft
 		void InsertLeft(nullptr_t);
@@ -134,7 +139,7 @@ namespace ld
 		// Break all linkship(not include childs ptr)
 		void BreakLinkship();
 #pragma endregion
-	private:
+		FourWayPointerLevel:
 #pragma region
 		// Rebuild index stats from this ptr to tail on this branch
 		// Best time to use this function rebuild index, this ptr need to be the head one
@@ -145,10 +150,23 @@ namespace ld
 		// return farthest leaf ptr depth
 		size_t RebuildDepthIndex();
 #pragma endregion
-	private:
+		FourWayPointerLevel:
 #pragma region
-
+		tag* GetPrecursor() const;
+		tag* GetSuccessor() const;
 #pragma endregion
+	public:
+		~FourWayPointer()
+		{
+			for (tag* i = this->childs_begin_ptr; i; i = i->right_prt)
+			{
+				delete i;
+			}
+			if (this->left_ptr)
+				this->left_ptr->right_prt = this->right_prt;
+			if (this->right_prt)
+				this->right_prt->left_ptr = this->left_ptr;
+		}
 	};
 
 #pragma region FourWay Pointer
@@ -442,84 +460,234 @@ namespace ld
 		return result;
 	}
 
+	template<typename T>
+	FourWayPointer<T>::tag* FourWayPointer<T>::GetPrecursor() const
+	{
+		return this->beyond_back_ptr;
+	}
+	template<typename T>
+	FourWayPointer<T>::tag* FourWayPointer<T>::GetSuccessor() const
+	{
+		return this->beyond_next_ptr;
+	}
+
 #pragma endregion
 
+	_LF_C_API(OClass) DefaultContainerMemory
+	{
+	public:
+		static void* Malloc(size_t size);
+		static void Free(void* ptr, size_t size);
+		template<typename T>
+		using ptr_tag = T;
+	};
+
+	void* DefaultContainerMemory::Malloc(size_t size)
+	{
+		return ::malloc(size * sizeof(char));
+	}
+	void DefaultContainerMemory::Free(void* ptr, size_t size)
+	{
+		::free(ptr);
+	}
+
+	template<typename _Key,typename _Value,typename Memory>
+	class pair
+	{
+	public:
+		friend LFContainter;
+		using memory_indictaor = Memory;
+		using Key = memory_indictaor::ptr_tag<_Key>;
+		using Value = memory_indictaor::ptr_tag<_Value>;
+	private:
+		Key* key_ptr = nullptr;
+		Value* value_ptr = nullptr;
+		bool is_key_intrusive = false;
+		bool is_value_intrusive = false;
+	public:
+		pair(Key* key = nullptr, Value* value = nullptr) :key_ptr(key), value_ptr(value), is_key_intrusive(key != nullptr), is_value_intrusive(value != nullptr) {}
+		~pair()
+		{
+			if (!is_key_intrusive)delete key_ptr;
+			if (!is_value_intrusive)delete value_ptr;
+		}
+		const Key& GetKey()
+		{
+			if (key_ptr == nullptr)
+			{
+				is_key_intrusive = false;
+				key_ptr = new Key();
+			}
+			return *key_ptr;
+		}
+		Value& GetValue()
+		{
+			if (value_ptr == nullptr)
+			{
+				is_value_intrusive = false;
+				value_ptr = new Value();
+			}
+			return *value_ptr;
+		}
+		void SetValue(Value& value)
+		{
+			GetValue() = value;
+		}
+		void SetValue(Value* value)
+		{
+			if (!is_value_intrusive)delete value_ptr;
+			is_value_intrusive = true;
+			value_ptr = value;
+		}
+		operator const Key& ()
+		{
+			return GetKey();
+		}
+		operator Value& ()
+		{
+			return GetValue();
+		}
+	};
+
 	template<
-		typename Key, typename Value, typename IndexOrderIndicator, typename MemoryIndicator,
-		typename InsertIndicator, typename EarseIndicator
+		typename Key, typename Value,
+		class MemoryIndicator,
+		class Controller
 	>
-	//*
-	//	
-	//*
 	_LF_C_API(TClass) LFContainter
 	{
 	public:
-		using index_indicator = IndexOrderIndicator;
+		// Key and Value: will be build up in target pair type, and be able to create intrusive slot or just new instance(it can free by itself)
+		using key_tag = Key;
+		// Key and Value: will be build up in target pair type, and be able to create intrusive slot or just new instance(it can free by itself)
+		using value_tag = Value;
+		// MemoryIndicator: static memeory manager, to control memeory and buffer
+		// -> public static void* Malloc(size_t size) const;
+		// -> public static void Free(void* ptr, size_t size) const;
 		using memory_indictaor = MemoryIndicator;
-		using insert_indicator = InsertIndicator;
-		using earse_indicator = EarseIndicator;
+		// Controller: constructor with no argument, control all the behaviour
+		// -> public node* ReadFromRoot(node* root, node* leaf, const Key& key) const;
+		// -> public node* QuickDetectContain(node* root, const Key& key) const
+		// -> public node* GenerateToRoot(node*& root, node*& leaf, const Key& key, node* new_ptr);
+		// -> public node* RemoveFromRoot(node* root, node* leaf, const Key& key)
+		// -> public template<_Ty> node* RemoveFromRoot(node* root, _Ty)
+		using controller_type = Controller;
 		LFContainter() :root(nullptr), leaf(nullptr)
 		{
 
 		}
-
-		class pair
+		LFContainter(LFContainter&) = delete;
+		LFContainter(LFContainter&& right) :root(right.root), leaf(leaf), my_controller(right.my_controller) noexcept {}
+		~LFContainter()
 		{
-		public:
-			friend LFContainter;
-		private:
-			Key* key_ptr = nullptr;
-			Value* value_ptr = nullptr;
-			bool is_key_intrusive = false;
-			bool is_value_intrusive = false;
-		public:
-			pair(Key* key = nullptr, Value* value = nullptr) :key_ptr(key), value_ptr(value), is_key_intrusive(key != nullptr), is_value_intrusive(value != nullptr) {}
-			~pair()
-			{
-				if (!is_key_intrusive)delete key_ptr;
-				if (!is_value_intrusive)delete value_ptr;
-			}
-			const Key& GetKey()
-			{
-				if (key_ptr == nullptr)
-				{
-					is_key_intrusive = false;
-					key_ptr = new Key();
-				}
-				return *key_ptr;
-			}
-			Value& GetValue()
-			{
-				if (value_ptr == nullptr)
-				{
-					is_value_intrusive = false;
-					value_ptr = new Value();
-				}
-				return *value_ptr;
-			}
-			void SetValue(Value& value)
-			{
-				GetValue() = value;
-			}
-			void SetValue(Value* value)
-			{
-				if (!is_value_intrusive)delete value_ptr;
-				is_value_intrusive = true;
-				value_ptr = value;
-			}
-			operator const Key& ()
-			{
-				return GetKey();
-			}
-			operator Value& ()
-			{
-				return GetValue();
-			}
-		};
-		using node = FourWayPointer<pair>;
+			this->ClearTree();
+		}
+
+		using pair_type = ld::pair<Key, Value, memory_indictaor>;
+		using node = FourWayPointer<pair_type>;
 	private:
 		node* root = nullptr;
 		node* leaf = nullptr;
+		controller_type my_controller;
+	protected:
+		void ToolClear(node * _node) const
+		{
+			_node->data.SetValue(nullptr);
+			for (node* i = _node->GetChildsLeft(); i; i = i->GetRight())
+			{
+				ToolClear(i);
+			}
+		}
+	public:
+		bool Isolated(node * _node) const
+		{
+			if (_node == root)return false;
+			else return
+				_node->GetLeft() == nullptr &&
+				_node->GetRight() == nullptr &&
+				_node->GetParent() == nullptr &&
+				_node->GetChildsLeft() == nullptr;
+		}
+		bool Contains(node * _node) const
+		{
+			return _node->GetRoot() == root;
+		}
+		bool IsEmpty() const
+		{
+			return root == nullptr;
+		}
+		void ClearTree()
+		{
+			delete root;
+		}
+		void ClearValue()
+		{
+			ToolClear(root);
+		}
+
+		bool Contains(const Key& key) const
+		{
+			return my_controller.QuickDetectContain(root, key);
+		}
+		Value& GetValue(const Key & key) const
+		{
+			node* result = my_controller.ReadFromRoot(root, leaf, key);
+			if (result == nullptr)
+			{
+				result = my_controller.GenerateToRoot(root, leaf, key, (node*)memory_indictaor::Malloc(sizeof(node)));
+			}
+			return result->data;
+		}
+		Value& GetValue(Key * key) const
+		{
+			node* result = my_controller.ReadFromRoot(root, leaf, key);
+			if (result == nullptr)
+			{
+				result = my_controller.GenerateToRoot(root, leaf, *key, (node*)memory_indictaor::Malloc(sizeof(node)));
+			}
+			return result->data;
+		}
+		void Remove(const Key & key)
+		{
+			memory_indictaor::Free(my_controller.RemoveFromRoot(root, leaf, key), sizeof(node));
+		}
+		void SetValue(const Key & key, Value & value) const
+		{
+			GetValue(key) = value;
+		}
+		void SetValue(const Key & key, Value && value) const
+		{
+			GetValue(key) = std::move(value);
+		}
+		void SetValue(Key * key, Value & value) const
+		{
+			GetValue(*key) = value;
+		}
+		void SetValue(Key * key, Value && value) const
+		{
+			GetValue(*key) = std::move(value);
+		}
+
+		node* GetNode(const Key& key) const
+		{
+			return my_controller.ReadFromRoot(root, leaf, key);
+		}
+	};
+
+	template<typename Key,typename Value,class Memory>
+	_LF_C_API(OClass) DefaultController
+	{
+		using pair_type = ld::pair<Key, Value, memory_indictaor>;
+	public:
+		using node = FourWayPointer<pair_type>;
+		node* ReadFromRoot(node * root, node * leaf, const Key & key) const
+		{
+
+		}
+		node* QuickDetectContain(node * root, const Key & key) const;
+		node* GenerateToRoot(node*& root, node*& leaf, const Key& key, node* new_ptr);
+		node* RemoveFromRoot(node* root, node* leaf, const Key& key)
+		template<_Ty> node* RemoveFromRoot(node* root, _Ty)
 	};
 }
 
