7f371c56ec9d1e50c2fb26c36d720b79375a5123
NINEMINEsigma <1371605831@qq.com>
2024-07-09 01:07:43 +0800

    +LDEvent

--- LightDiagram/Core/Header/LF_Architecture.h
+++ LightDiagram/Core/Header/LF_Architecture.h
@@ -19,7 +19,7 @@ namespace ld
     // the instance will be generate, but it will be never delete
     // Generate Process ->new TargetArch() -> Init()
     template<typename TargetArch>
-    TargetArch&     _LF_C_API(TDLL) ArchitectureInstance()
+    TargetArch& _LF_C_API(TDLL) ArchitectureInstance()
     {
         static_assert(std::is_base_of_v<IArchitecture, TargetArch>, "TargetArch need be architecture");
         if (ArchitectureInstance(typeid(TargetArch)) == nullptr)
@@ -36,7 +36,7 @@ namespace ld
     }
 
     template<typename Arch, typename SlotType>
-    SlotType*       _LF_C_API(TDLL) RegisterOn(_In_ SlotType* component)
+    SlotType* _LF_C_API(TDLL) RegisterOn(_In_ SlotType* component)
     {
         IArchitecture& arch = *ArchitectureInstance(typeid(Arch));
         arch.Register(typeid(SlotType), component);
@@ -83,8 +83,11 @@ namespace ld
         arch.Diffusing(typeid(SlotType));
     }
 
+    //*
+    //  When Func is <Returner>(<void>), OnExecute[send command] is enable to call Listener 
+    //*
     template<typename Func>
-    _LF_C_API(TClass) 
+    _LF_C_API(TClass)
         LDEvent Symbol_Push
         _LF_Inherited(ICommand)
     {
@@ -98,17 +101,17 @@ namespace ld
             functions.push_back(func);
             return *this;
         }
-        LDEvent& AddListener(const LDEvent& event)
+        LDEvent& AddListener(const LDEvent & event)
         {
             for (auto& i : event.functions) AddListener(i);
             return *this;
         }
-        LDEvent& AddListener(const func_container& event)
+        LDEvent& AddListener(const func_container & event)
         {
             for (auto& i : event) AddListener(i);
             return *this;
         }
-        LDEvent& RemoveListener(const tag& func)
+        LDEvent& RemoveListener(const tag & func)
         {
             functions.erase(std::find(functions.begin(), functions.end(), func));
             return *this;
@@ -128,7 +131,10 @@ namespace ld
         using consting = typename traits::consting;
         using typen = typename traits::typen;
 
-        using OnInvokeResult = typename choose_type<std::is_same_v<result, void>, void, std::vector<result>>::tag;
+        using OnInvokeResult = typename choose_type<std::is_same_v<result, void>, void, std::vector<result>&>::tag;
+        using HoldInvokeResult = typename choose_type<std::is_same_v<OnInvokeResult, void>, bool, OnInvokeResult>::tag;
+
+        typename generate_full_decay_typen<HoldInvokeResult>::unrefer result_data;
 
         template<typename... Args>
         OnInvokeResult OnInvoke(Args... args)
@@ -142,25 +148,24 @@ namespace ld
             }
             else
             {
-                std::vector<result> returner;
                 for (auto& i : this->functions)
                 {
-                    returner.push_back(i(args...));
+                    result_data.push_back(i(args...));
                 }
-                return returner;
+                return result_data;
             }
         }
 
         LDEvent() {}
-        LDEvent(const tag& func)
+        LDEvent(const tag & func)
         {
             AddListener(func);
         }
-        LDEvent(LDEvent& right)noexcept
+        LDEvent(LDEvent & right)noexcept
         {
             this->functions = right.functions;
         }
-        LDEvent(LDEvent&& right) noexcept
+        LDEvent(LDEvent && right) noexcept
         {
             this->functions = std::move(right.functions);
         }
@@ -170,7 +175,51 @@ namespace ld
         }
         virtual void OnExecute() override
         {
+            if constexpr (std::is_same_v<parameters, type_list<void>>)
+            {
+                this->Invoke();
+            }
+        }
 
+        template<typename... Args>
+        OnInvokeResult operator()(Args... args)
+        {
+            this->OnInvoke(args...);
+            if constexpr (std::is_same_v<OnInvokeResult, void>)
+            {
+                return result_data;
+            }
+        }
+
+        void operator+=(const tag & func)
+        {
+            this->AddListener(func);
+        }
+        void operator+=(const LDEvent & event)
+        {
+            this->AddListener(event);
+        }
+        void operator+=(const func_container & event)
+        {
+            this->AddListener(event);
+        }
+        void operator-=(const tag & func)
+        {
+            this->RemoveListener(func);
+        }
+
+        template<typename Array>
+        void sort(const Array & order)
+        {
+            if constexpr (!std::is_same_v<OnInvokeResult, void>)
+            {
+                decltype(result_data) new_data;
+                for (auto& i : order)
+                {
+                    new_data.push_bask(result_data[i]);
+                }
+                result_data = new_data;
+            }
         }
     };
 }
