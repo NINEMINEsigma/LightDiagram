43135ec3eb01b3777c63fd4c3e61a0a0060dfd48
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2024-07-08 10:26:27 +0800

    FileSystem.h已弃用

--- LightDiagram/Utility/FileSystem.h
+++ LightDiagram/Utility/FileSystem.h
@@ -1,3 +1,4 @@
+#define __FILE_FILESYSTEM
 #ifndef __FILE_FILESYSTEM
 #define __FILE_FILESYSTEM
 
@@ -12,13 +13,42 @@ namespace ld
 		using wstring = std::wstring;
 
 		constexpr wchar_t separator = L'/';
+		constexpr wchar_t separator2 = L'\\';
+
+		size_t FindPathSepartor(const string& str)
+		{
+			size_t result = str.find(separator);
+			return result == string::npos ? str.find(separator2) : result;
+		}
+		size_t rFindPathSepartor(const string& str)
+		{
+			size_t result = str.rfind(separator);
+			return result == string::npos ? str.rfind(separator2) : result;
+		}
+		size_t FindPathSepartor(const wstring& str)
+		{
+			size_t result = str.find(separator);
+			return result == string::npos ? str.find(separator2) : result;
+		}
+		size_t rFindPathSepartor(const wstring& str)
+		{
+			size_t result = str.rfind(separator);
+			return result == string::npos ? str.rfind(separator2) : result;
+		}
 
 		struct any_info
 		{
-			// Path's attribute
+		public:
 			wstring name, path;
+			// Path's attribute
 			virtual wstring GetFullPath() const abstract;
 			virtual bool IsExist() const abstract;
+		private:
+			any_info(const wstring& fullpath, int sp, bool coren) :path(coren ? L"" : fullpath.substr(0, sp)), name(coren ? fullpath : fullpath.substr(sp + 1, fullpath.size() - sp - 1)) {}
+			any_info(const wstring& fullpath, int sp) :any_info(fullpath,sp, sp == wstring::npos) {}
+		public:
+			any_info() :name(L""), path(L"") {}
+			any_info(const wstring& fullpath) :any_info(fullpath, rFindPathSepartor(fullpath)) {}
 		};
 
 		wstring Combine(const wstring& path, const wstring& next)
@@ -30,14 +60,20 @@ namespace ld
 		{
 			struct Info :public any_info
 			{
+				Info():extension(L"") {}
 				wstring extension;
+			private:
+				Info(const wstring& fullpath, int sp, bool coren) :any_info(fullpath.substr(0, sp)), extension(coren ? L"" : fullpath.substr(sp + 1, fullpath.size() - sp - 1)) {}
+				Info(const wstring& fullpath, int sp) :Info(fullpath, sp, sp == wstring::npos) {}
+			public:
+				Info(const wstring& fullpath) :Info(fullpath, fullpath.rfind('.')) {}
 				wstring GetFullPathWithoutExtension() const
 				{
 					return path + separator + name;
 				}
 				virtual wstring GetFullPath() const override
 				{
-					return GetFullPathWithoutExtension() + L'.' + extension;
+					return GetFullPathWithoutExtension() + (extension.size() ? (L'.' + extension) : L"");
 				}
 				virtual bool IsExist() const override
 				{
@@ -45,7 +81,10 @@ namespace ld
 				}
 			};
 			Info fileinfo;
-
+			operator Info& ()
+			{
+				return fileinfo;
+			}
 			operator wstring() const
 			{
 				return this->fileinfo.GetFullPath();
@@ -63,6 +102,7 @@ namespace ld
 				else return -1;
 			}
 
+			File(const wstring& fullpath) :fileinfo(fullpath) {}
 		};
 
 
@@ -70,6 +110,7 @@ namespace ld
 		{
 			struct Info :public any_info
 			{
+				Info(const wstring& fullpath) :any_info(fullpath) {}
 				virtual wstring GetFullPath() const override
 				{
 					return path + separator + name;
@@ -81,13 +122,26 @@ namespace ld
 				}
 			};
 			Info dirinfo;
-			std::vector<File> files;
+			using subfiles_shared_type = std::shared_ptr<File>;
+			using subfiles_container = std::vector<subfiles_shared_type>;
+			using subdir_shared_type = std::shared_ptr<Directory>;
+			using subdir_container = std::vector<subdir_shared_type>;
+			subfiles_container files;
+			subdir_container dirs;
 
-			operator wstring()
+			operator Info& ()
+			{
+				return dirinfo;
+			}
+			operator subfiles_container& ()
+			{
+				return files;
+			}
+			operator wstring() const
 			{
 				return this->dirinfo.GetFullPath();
 			}
-			operator bool()
+			operator bool() const
 			{
 				return dirinfo.IsExist();
 			}
@@ -124,29 +178,41 @@ namespace ld
 			void RefreshFileList()
 			{
 				files.clear();
+				dirs.clear();
 #if defined(_WINDOW_)||defined(_LINUX_ON_WINDOW_)
 				struct _finddata_t fileinfo;
 				intptr_t handle = _findfirst(to_string(dirinfo.GetFullPath()).c_str(), &fileinfo);
 				if (handle == -1) return;
 				while (!_findnext(handle, &fileinfo))
 				{
-					//files.push_back(File())
+					files.push_back(std::make_shared<File>(new File(to_wstring(fileinfo.name))));
 				}
 				_findclose(handle);
 #else
 				int fileNum = 0;
 				DIR* pDir;
 				struct dirent* ptr;
-				if (!(pDir = opendir(path.c_str())))
-					return fileNum;
+				if (!(pDir = opendir(path.c_str())))return;
 				while ((ptr = readdir(pDir)) != 0)
 				{
 					if (strcmp(ptr->d_name, ".") != 0 && strcmp(ptr->d_name, "..") != 0)
-						fileNum++;
+						files.push_back(std::make_shared<File>(new File(to_wstring(ptr->d_name)));
 				}
 				closedir(pDir);
 #endif
+				if (this->dirinfo.IsExist())
+				{
+					for (auto& i : std::filesystem::directory_iterator(this->dirinfo.GetFullPath()))
+					{
+						if (std::filesystem::is_directory(i))
+						{
+							//files.push_back(std::make_shared<File>(new File(to_wstring(i.path().native()))));
+						}
+					}
+				}
 			}
+
+			Directory(const wstring& fullpath) :dirinfo(fullpath) {}
 		};
 	};
 }
