3af5db83f2bcc214371a213c9c15f3e7fc6dd6c8
NINEMINEsigma <1371605831@qq.com>
2024-07-16 15:24:55 +0800

    Update LF_RAII.h
    +ID instance

--- LightDiagram/Core/Header/LF_RAII.h
+++ LightDiagram/Core/Header/LF_RAII.h
@@ -17,6 +17,7 @@ namespace ld
 {
 	template<typename T> _LF_C_API(Class) instance;
 
+	// Referance Counter
 	template<> _LF_C_API(Class) instance<void> Symbol_Push _LF_Inherited(any_class)
 	{
 	private:
@@ -56,6 +57,7 @@ namespace ld
 			this->set_counter(nullptr);
 		}
 	public:
+		using tag = void;
 		instance() :instance_counter(new size_t(1)) {}
 		instance(instance & from) noexcept :instance_counter(from.instance_counter)
 		{
@@ -74,7 +76,7 @@ namespace ld
 				(*this->instance_counter)--;
 			}
 		}
-		size_t ref_count() const
+		size_t ref_count() const noexcept
 		{
 			return *this->instance_counter;
 		}
@@ -111,18 +113,22 @@ namespace ld
 		}
 	};
 
+	using instance_base = instance<void>;
+
+	// Referance Counter(has function <get_ptr> and <get_ref>)
 	template<> _LF_C_API(Class) instance<nullptr_t> Symbol_Push _LF_Inherited(instance<void>)
 	{
 	public:
+		using tag = nullptr_t;
 		instance() :instance<void>() {}
 		instance(instance & from) noexcept :instance<void>(from) {}
 		instance(instance && from) noexcept :instance<void>(std::move(from)) {}
 		virtual ~instance() {}
-		void* ref_ptr() noexcept
+		void* get_ptr() const noexcept
 		{
 			return nullptr;
 		}
-		nullptr_t ref_ins()
+		nullptr_t get_ref() const throw(LDException)
 		{
 			throw ld::LDException("bad function");
 		}
@@ -150,6 +156,8 @@ namespace ld
 		}
 	};
 
+	using instance_counter = instance<nullptr_t>;
+
 	template<typename Tag> _LF_C_API(TClass) instance Symbol_Push _LF_Inherited(instance<void>)
 	{
 		Tag* instance_ptr;
@@ -158,6 +166,18 @@ namespace ld
 		{
 			delete this->instance_ptr;
 		}
+#pragma region instance_ptr Property
+		Tag* get_ptr()const
+		{
+			return this->instance_ptr;
+		}
+		Tag* set_ptr(Tag * inptr)
+		{
+			Tag* result = this->instance_ptr;
+			this->instance_ptr = inptr;
+			return result;
+		}
+#pragma endregion
 	public:
 		instance(Tag * ptr) :instance_ptr(ptr), instance<void>() {}
 		instance() :instance<Tag>(nullptr) {}
@@ -176,14 +196,16 @@ namespace ld
 		instance<Tag>& operator=(instance<Tag>&from) noexcept
 		{
 			instance<void>::operator=(from);
+			this->set_ptr(from.instance_ptr);
 			return *this;
 		}
 		instance<Tag>& operator=(instance<Tag> && from) noexcept
 		{
+			this->set_ptr(std::move(from.instance_ptr));
 			instance<void>::operator=(std::move(from));
 			return *this;
 		}
-		bool operator==(const instance<nullptr_t>&from) const noexcept
+		bool operator==(const instance<Tag>&from) const noexcept
 		{
 			return instance<void>::operator==(from);
 		}
@@ -192,7 +214,7 @@ namespace ld
 	template<size_t Max> _LF_C_API(Class) instance<ConstexprCount<Max>> Symbol_Push _LF_Inherited(instance<nullptr_t>)
 	{
 	private:
-		void CheckStatus()const
+		void CheckStatus() const throw(LDException)
 		{
 			if (this->ref_count() >= Max)
 				throw ld::LDException("over count");
@@ -208,16 +230,71 @@ namespace ld
 		{
 
 		}
-
+		instance<Tag>& operator=(instance<Tag>&from) noexcept
+		{
+			instance<nullptr_t>::operator=(from);
+			return *this;
+		}
+		instance<Tag>& operator=(instance<Tag> && from) noexcept
+		{
+			instance<nullptr_t>::operator=(std::move(from));
+			return *this;
+		}
+		bool operator==(const instance<ConstexprCount<Max>>&from) const noexcept
+		{
+			return instance<nullptr_t>::operator==(from);
+		}
 	};
 
-	template<typename Tag, size_t SlotID> instance<SlotMode<Tag, SlotID>> Symbol_Push _LF_Inherited(any_class)
+	template<size_t Max> using instance_limit = instance<ConstexprCount<Max>>;
+
+	template<typename Tag, size_t SlotID> _LF_C_API(Class) instance<SlotMode<Tag, SlotID>> Symbol_Push _LF_Inherited(any_class)
 	{
-		static instance<Tag> single_instance;
+		static instance* hoster;
+		static Tag* single_ptr;
 	public:
+		instance(Tag * ptr)
+		{
+			delete instance::single_ptr;
+			instance::single_ptr = ptr;
+			instance::hoster = this;
+		}
+		instance() {}
+		instance(instance && from) noexcept
+		{
+			if (instance::hoster == &from)
+				instance::hoster = this;
+		}
+		virtual ~instance()
+		{
+			if (this == instance::hoster)
+				delete instance::single_ptr;
+		}
 
+		Tag* ref_ptr() noexcept
+		{
+			return instance::single_ptr;
+		}
+		Tag& ref_ins() noexcept
+		{
+			return *instance::single_ptr;
+		}
+		instance<Tag>& operator=(instance<Tag> && from)  noexcept
+		{
+			if (instance::hoster == &from)
+				instance::hoster = this;
+			return *this;
+		}
+		template<typename AnyTag, size_t AnyID>
+		constexpr bool operator==(const instance<SlotMode<AnyTag, AnyID>>&from) const noexcept
+		{
+			return std:; is_same_v<AnyTag, Tag>&& AnyID == SlotID;
+		}
 	};
-	template<typename Tag, size_t SlotID> instance<Tag> instance<SlotMode<Tag, SlotID>>::single_instance;
+
+	template<typename Tag, size_t SlotID> Tag* instance<SlotMode<Tag, SlotID>>::single_ptr = nullptr;
+	template<typename Tag, size_t SlotID> instance<SlotMode<Tag, SlotID>>* instance<SlotMode<Tag, SlotID>>::hoster = nullptr;
+	template<typename Tag, size_t SlotID>  using instance_static = instance<SlotMode<Tag, SlotID>>;
 }
 
 #endif // !__FILE_LF_RAII
