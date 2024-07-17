0fd93528fdca2744a39d348b7bf7602172aa06d3
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2024-07-17 22:17:59 +0800

    Update test.cpp

--- Test/test.cpp
+++ Test/test.cpp
@@ -25,7 +25,7 @@ struct inter :_LF_Inherited(any_class)
 	}
 };
 
-using test_type = meta_instance<type_list<ld_test>, type_list<int(void),int(void)>, type_list<int>>;
+using test_type = meta_instance<type_list<ld_test>, type_list<int(void)>, type_list<int,int>>;
 int __get(test_type* _this)
 {
 	return _this->_m_fields.get_value<0>();
@@ -40,8 +40,7 @@ int __get2(test_type* _this)
 int main()
 {
 	system("chcp 65001");
-	test_type testx(__get, __get2);
+	test_type testx(__get);
 	testx.set_value<0>(95);
 	cout << testx.invoke<0>() << "\n";
-	cout << testx.invoke<1>() << "\n";
 }
