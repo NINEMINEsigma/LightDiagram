642c62ee18163507da00d3eca7d1af6bd473d1c3
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2024-08-16 16:47:31 +0800

    Update CorrelationAnalysis.h

--- LightMath/CorrelationAnalysis.h
+++ LightMath/CorrelationAnalysis.h
@@ -19,20 +19,20 @@ namespace ld
 		 vectors A and B. This sum of squares indicates the degree of change in the strength
 		 of the relationship between the corresponding elements in the two vectors. By opening
 		 the square root, we can get the degree to which the strength of the relationship 
-		 between each element changes relative to the mean value
+		 between each element changes relative to the mean value.
 		 */
 		Number Pearson(const std::vector<Number>& sorted_data1,const std::vector<Number>& sorted_data2, long Length);
 		Number Pearson_T_value(const Number& r, const Integer& n);
 
 		/*
 		r = S / M.
+		 The implementation of this function takes a very simple hierarchical difference strategy
+		 and abandons the principle of averaging the same values.
+		 Therefore, when the repetition value exceeds 10%, the implementation of this function should
+		 be abandoned.When the value is repeated and is lower than this value, this function provides
+		 both high performance and a large degree of accuracy.
 		S = 6Σ(d_x-d_y)^2.
 		M = n(n^2 - 1).
-		The implementation of this function takes a very simple hierarchical difference strategy
-		and abandons the principle of averaging the same values.
-		Therefore, when the repetition value exceeds 10%, the implementation of this function should
-		be abandoned.When the value is repeated and is lower than this value, this function provides
-		both high performance and a large degree of accuracy
 		*/
 		Number Spearman(const std::vector<Number>& x, const std::vector<Number>& y);
 	}
