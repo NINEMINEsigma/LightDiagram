9e8d4c395cd5817d9dce309895688fd32ea63d09
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2024-09-17 21:57:13 +0800

    实现了在矩阵中的四向相邻DFS(正逆)

--- LightDiagram/Core/algorithm.h
+++ LightDiagram/Core/algorithm.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #ifndef __FILE_ALGORITHM
 #define __FILE_ALGORITHM
@@ -73,7 +73,7 @@ namespace ld
 	{
 		using iter_unwap = decltype(intervals.front());
 		using iter_unwap_lv = std::add_lvalue_reference_t<iter_unwap>;
-		std::sort(intervals.begin(), intervals.end(), []( 
+		std::sort(intervals.begin(), intervals.end(), [](
 			iter_unwap_lv _left, iter_unwap_lv _right)
 			{
 				return get_left(_left) < get_left(_right);
@@ -89,6 +89,54 @@ namespace ld
 				get_right(result.back()) = std::max(get_right(result.back()), get_right(item));
 		}
 		intervals = std::move(result);
+	}
+
+	namespace algorithm
+	{
+#define __Check__Mapper(x,y) ((x)>=0&&(y)>=0&&(y)<mapper.size()&&(x)<mapper[(y)].size())
+		template<typename _PosN, typename _Mapper, typename _Recorder, typename _Pr>
+		void DFS_Matrix_4(const _PosN& x, const _PosN& y, _Mapper& mapper, _Recorder& record, _Pr pred)
+		{
+			auto&& current = mapper[y][x];
+			record[y][x] = true;
+			if (__Check__Mapper(x, y + 1) && record[y + 1][x] == false && pred(current, mapper[y + 1][x], 0, 1))
+				DFS_Matrix_4(x, y + 1, mapper, record, pred);
+			if (__Check__Mapper(x, y - 1) && record[y - 1][x] == false && pred(current, mapper[y - 1][x], 0, -1))
+				DFS_Matrix_4(x, y - 1, mapper, record, pred);
+			if (__Check__Mapper(x + 1, y) && record[y][x + 1] == false && pred(current, mapper[y][x + 1], 1, 0))
+				DFS_Matrix_4(x + 1, y, mapper, record, pred);
+			if (__Check__Mapper(x - 1, y) && record[y][x - 1] == false && pred(current, mapper[y][x - 1], -1, 0))
+				DFS_Matrix_4(x - 1, y, mapper, record, pred);
+		}
+		template<typename _PosN, template<typename> class _Layer, typename _KeyTy, typename _Pr>
+		_Layer<_Layer<bool>> Get_DFS_Matrix_4(const _PosN& x, const _PosN& y, _Layer<_Layer<_KeyTy>> mapper, _Pr pred)
+		{
+			_Layer<_Layer<bool>> record(mapper.size(), _Layer<bool>(mapper.begin()->size(), false));
+			DFS_Matrix_4<_PosN, _Layer<_Layer<_KeyTy>>, _Layer<_Layer<bool>>, _Pr>(x, y, mapper, record, pred);
+			return record;
+		}
+		template<typename _PosN, typename _Mapper, typename _Recorder, typename _Pr>
+		void rDFS_Matrix_4(const _PosN& x, const _PosN& y, _Mapper& mapper, _Recorder& record, _Pr pred)
+		{
+			auto&& current = mapper[y][x];
+			record[y][x] = true;
+			if (__Check__Mapper(x, y + 1) && record[y + 1][x] == false && pred(mapper[y + 1][x], current, 0, -1))
+				rDFS_Matrix_4(x, y + 1, mapper, record, pred);
+			if (__Check__Mapper(x, y - 1) && record[y - 1][x] == false && pred(mapper[y - 1][x], current, 0, 1))
+				rDFS_Matrix_4(x, y - 1, mapper, record, pred);
+			if (__Check__Mapper(x + 1, y) && record[y][x + 1] == false && pred(mapper[y][x + 1], current, -1, 0))
+				rDFS_Matrix_4(x + 1, y, mapper, record, pred);
+			if (__Check__Mapper(x - 1, y) && record[y][x - 1] == false && pred(mapper[y][x - 1], current, 1, 0))
+				rDFS_Matrix_4(x - 1, y, mapper, record, pred);
+		}
+		template<typename _PosN, template<typename> class _Layer, typename _KeyTy, typename _Pr>
+		_Layer<_Layer<bool>> Get_rDFS_Matrix_4(const _PosN& x, const _PosN& y, _Layer<_Layer<_KeyTy>> mapper, _Pr pred)
+		{
+			_Layer<_Layer<bool>> record(mapper.size(), _Layer<bool>(mapper.begin()->size(), false));
+			rDFS_Matrix_4<_PosN, _Layer<_Layer<_KeyTy>>, _Layer<_Layer<bool>>, _Pr>(x, y, mapper, record, pred);
+			return record;
+		}
+#undef __Check__Mapper
 	}
 }
 
--- Test/test.cpp
+++ Test/test.cpp
@@ -4,26 +4,102 @@ using namespace ld::algorithm;
 
 using namespace std;
 
-class Solution
+auto ___auto___ = []()
+	{
+		cin.tie(nullptr)->sync_with_stdio(false), cout.tie(nullptr);
+		return 0;
+	};
+
+constexpr char wall = '#';
+constexpr char able = '+';
+constexpr char horizontal = '-';
+constexpr char vertical = '|';
+constexpr char onlydown = '.';
+constexpr char start = 'S';
+constexpr char target = 'T';
+
+bool check_path(const char& current, const char& next, const int& dx, const int& dy)
 {
-public:
-    int maxSubArray(vector<int>& nums) 
-    {
-        int m = nums[0];
-        for (int i = 1; i < nums.size(); i++) 
-        {
-            nums[i] = max(nums[i], nums[i] + nums[i - 1]);
-            m = max(m, nums[i]);
-        }
-        return m;
-    }
-};
+	if (next == wall)
+		return false;
+	switch (current)
+	{
+	case wall:
+		return false;
+	case horizontal:
+		return dy == 0;
+	case vertical:
+		return dx == 0;
+	case onlydown:
+		return dy == 1;
+	default:
+		return true;
+	}
+}
 
 int main()
 {
-	Solution s;
-	vector<int> nums = { -2,1,-3,4,-1,2,1,-5,4 };
-	auto a = s.maxSubArray(nums);
-	cout << a;
+	//init
+	int r, c;
+	cin >> r >> c;
+	vector<vector<char>> mapper(r, vector<char>(c));
+	int startx, starty, targetx, targety;
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			char current;
+			cin >> current;
+			mapper[i][j] = current;
+			if (current == start)
+			{
+				starty = i;
+				startx = j;
+			}
+			else if (current == target)
+			{
+				targety = i;
+				targetx = j;
+			}
+		}
+	}
+	//dfs
+	auto start_record = Get_DFS_Matrix_4<
+		int, vector, char, function<bool(const char&, const char&, const int&, const int&)>
+	>(startx, starty, mapper, function(check_path));
+	auto target_record = Get_rDFS_Matrix_4<
+		int, vector, char, function<bool(const char&, const char&, const int&, const int&)>
+	>(targetx, targety, mapper, function(check_path));
+	//check
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			if (start_record[i][j] && target_record[i][j])
+				cout << ConsoleColor::Yellow << mapper[i][j];
+			else if (start_record[i][j] && !target_record[i][j])
+				cout << ConsoleColor::Green << mapper[i][j];
+			else if (!start_record[i][j] && target_record[i][j])
+				cout << ConsoleColor::Red << mapper[i][j];
+			else
+				cout << ConsoleColor::None << mapper[i][j];
+			cout << " ";
+		}
+		cout << "\n";
+	}
+	cout << ConsoleColor::None;
+	//result
+	int res = 0;
+	if (start_record[targety][targetx])
+	{
+		for (int i = 0; i < r; i++)
+		{
+			for (int j = 0; j < c; j++)
+			{
+				if (start_record[i][j] && !target_record[i][j]) res++; 
+			}
+		}
+		cout << res;
+	}
+	else cout << "I'm stuck!";
 }
-
